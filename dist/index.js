990fe77ab01d107c4b3afb55d47e91d493e3af12
wanxianliang <wanxianliang@yixin.im>
2023-03-11 21:37:06 +0800

    System commit

--- dist/index.js
+++ dist/index.js
@@ -2,36 +2,39 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_native_1 = require("react-native");
 var ReactNativeFileStreamModule = react_native_1.NativeModules.ReactNativeFileStreamModule;
-var eventEmitter = new react_native_1.NativeEventEmitter(ReactNativeFileStreamModule);
-eventEmitter.addListener("log_from_native", function (data) {
-    console.log("log_from_native", data);
-});
-eventEmitter.addListener("file_read_data", function (data) {
-    var key = data === null || data === void 0 ? void 0 : data.key;
-    var base64Data = data === null || data === void 0 ? void 0 : data.base64Data;
-    if (!key || !data) {
-        console.log("read file error", data);
-    }
-    console.log("收到文件信息", base64Data.length);
-    var callBack = ReactNativeFileStream.callBackMap.get(key);
-    if (callBack) {
-        callBack.call(null, {
-            data: base64Data,
-            hasMore: true
-        });
-    }
-});
-eventEmitter.addListener("file_Read_finish", function (data) {
-    console.log('完成数据读取');
-    var key = data === null || data === void 0 ? void 0 : data.key;
-    var callBack = ReactNativeFileStream.callBackMap.get(key);
-    if (callBack) {
-        callBack.call(null, {
-            data: null,
-            hasMore: false
-        });
-    }
-});
+// const eventEmitter = new NativeEventEmitter(ReactNativeFileStreamModule);
+// eventEmitter.addListener("log_from_native", data => {
+//     console.log("log_from_native", data);
+// });
+// eventEmitter.addListener("file_read_data", (data: {
+//     key: string,
+//     base64Data: string
+// }) => {
+//     const key = data?.key;
+//     const base64Data = data?.base64Data;
+//     if (!key || !data) {
+//         console.log("read file error", data);
+//     }
+//     console.log("收到文件信息", base64Data.length);
+//     const callBack = ReactNativeFileStream.callBackMap.get(key);
+//     if (callBack) {
+//         callBack.call(null, {
+//             data: base64Data,
+//             hasMore: true
+//         });
+//     }
+// });
+// eventEmitter.addListener("file_Read_finish", data => {
+//     console.log('完成数据读取');
+//     const key = data?.key;
+//     const callBack = ReactNativeFileStream.callBackMap.get(key);
+//     if (callBack) {
+//         callBack.call(null, {
+//             data: null,
+//             hasMore: false
+//         });
+//     }
+// });
 var ReadContext = /** @class */ (function () {
     function ReadContext() {
         this.key = "";
@@ -49,8 +52,6 @@ var ReactNativeFileStream = /** @class */ (function () {
     function ReactNativeFileStream() {
     }
     ReactNativeFileStream.initReadFile = function (file, bufferSize, cb) {
-        console.log("sssssssssssssssssss", file);
-        throw new Error("ssss");
         var uri = file.uri;
         var key = file.fileName + file.fileSize;
         this.callBackMap.set(key, cb);
