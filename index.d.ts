cd73e6e5a20eebe26a4e077385068c90f71df22d
wanxianliang <wanxianliang@yixin.im>
2023-03-11 21:10:04 +0800

    System commit

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,14 @@
+declare class ReadContext {
+    key: string;
+    static newReadContext(key: string): ReadContext;
+    listener(eventName: "data" | "finish", cb: Function): void;
+}
 export default class ReactNativeFileStream {
     static callBackMap: Map<any, any>;
-    static readFileAsBuffer(file: {
+    static initReadFile(file: {
         uri: string;
         fileName: string;
         fileSize: number;
-    }, bufferSize: number, cb: Function<string>): Promise<void>;
+    }, bufferSize: number, cb: Function): ReadContext | undefined;
 }
+export {};
